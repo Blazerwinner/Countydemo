c8b2b62fbb427d54b311a4ba8b782864f580fdb9
Blazerwinner <Brwinner0@gmail.com>
2020-01-05 17:43:34 -0800

    new

--- webapp.py
+++ webapp.py
@@ -2,32 +2,34 @@ from flask import Flask, request, Markup, render_template, flash, Markup
 import os
 import json
 
+_name_ = '_main_'
+
 app = Flask(_name_)
 
 @app.route("/")
-def render_main();
+def render_main():
         with open('county_demographics.json') as demographics_data:
                 counties = json.load(demographics_data)
         if 'states' in request.args:
                 return render_template('home.html', states = get_state_options(counties), highestTravelTime = fun_fact(request.args['states'], counties))
         else:
-                return render_template(' home.html', states = get_state_options(counties))
+                return render_template('home.html', states = get_state_options(counties))
      
 def get_state_options(counties):
         listOfStates = []
         for data in counties:
                 if data['State'] not in listOfStates:
-                        listOfStates.append(data['State])
+                        listOfStates.append(data['State'])
         options = ""
         for data in listOfStates:
-                options = options + Markup("<option value=/""+data+"/">"+data+"</option>")
+                options = options + Markup('<option value="'+data+'">'+data+'</option>')
         return options
 def fun_fact(state, counties):
         highestTravelTime = ["county", 0.0]
         for data in counties:
                 if data['State'] == state:
-                        if highestTravelTime[1] < data['Miscellaneous'] ['Mean Building Permits']:
-                                highestTravelTime[1] = data['Miscellaneous'] ['Mean Building Permits']
+                        if highestTravelTime[1] < data['Miscellaneous'] ['Building Permits']:
+                                highestTravelTime[1] = data['Miscellaneous'] ['Building Permits']
                                 highestTravelTime[0] = data['County']
         return highestTravelTime
 if _name_=="_main_":
